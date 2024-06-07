65f8b5ab33e331f428d5229f5a56e62862f05ec1
jobayer <mdjobayerarafat@gmail.com>
2024-06-07 11:24:35 -0400

    valid2

--- src/validParenthese.rs
+++ src/validParenthese.rs
@@ -4,5 +4,26 @@ impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut strack = Vec::new();
         let mapping = vec![(')', '(', ']', '[', '}', '{')];
+        for c in s.chars(){
+            if c == '(' || c =='{' || c == '[' {
+                stack.push(c);
+            } else {
+                if stack.is_empty(){
+                    return false;
+                }
+                let top = stack.pop().unwrap();
+                let mut found = false;
+                for &(close, open) in &mapping {
+                    if c == close && top == open {
+                        found = true;
+                        break;
+                    }
+                }
+                if !found {
+                    return false;
+                }
+            }
+        }
+        strack.is_empty()
     }
 }
