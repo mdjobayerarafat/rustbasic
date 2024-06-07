f1d794db6640c6355d45107f89972ab6cadc139c
jobayer <mdjobayerarafat@gmail.com>
2024-06-07 11:30:09 -0400

    alter1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod validParenthese;
+mod mergeStringAlternately;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/mergeStringAlternately.rs
@@ -0,0 +1,4 @@
+// you are given two strings word1 and word2. Merge the string bby adding letters in alternating order, string with word1. if a string is longer than other, append the additional letters onto the end of the merged string.
+impl Solution {
+    pub fn merge_alternatelty
+}
